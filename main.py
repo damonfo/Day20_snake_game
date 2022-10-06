3864274432a953d5899c60a5594712ede3761866
damonfo <damonfoo.au@gmail.com>
2022-10-06 17:30:29 +1000

    Game over feature and set bundary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import time
+import turtle
 from turtle import Screen
+
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 
@@ -8,17 +11,18 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Classic Snake Game")
-screen.tracer(0)    # 关闭动画效果
+screen.tracer(0)  # 关闭动画效果
 # Create a snake body
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 # Move the snake
 game_is_on = True
@@ -27,12 +31,14 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-# 倒着循环，让最后一个块移动到前一个块的位置上
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.score_add()
 
-
-
-
-
-
+    # Detect collision with the wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
